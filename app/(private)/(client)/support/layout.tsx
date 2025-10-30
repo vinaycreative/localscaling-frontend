d45cee096987dc47f113f36ea8b8f55ca8884838
Vinay <vnaycreative@gmail.com>
2025-10-30 14:26:41 +0530

    fix: minor import bug resolve

--- app/(private)/(client)/support/layout.tsx
+++
@@ -1,6 +0,0 @@
-import MainLayout from "@/components/layout/main"
-import React, { ReactNode } from "react"
-
-export default function page({ children }: { children: ReactNode }) {
-  return <MainLayout>{children}</MainLayout>
-}
