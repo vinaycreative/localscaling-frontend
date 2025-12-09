bbcd0462719c7c0736feb3e2b97a2f68e037b805
AdilAhmed976 <adilahmedah976@gmail.com>
2025-12-09 18:11:10 +0530

    fix : error message font size update

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -147,7 +147,7 @@ function FormMessage({ className, ...props }: React.ComponentProps<"p">) {
     <p
       data-slot="form-message"
       id={formMessageId}
-      className={cn("text-destructive text-sm", className)}
+      className={cn("text-destructive text-[12px]", className)}
       {...props}
     >
       {body}
