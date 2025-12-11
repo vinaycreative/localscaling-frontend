f4aaf9688ed177c3b367e4c8cf67183001128fb4
AdilAhmed976 <adilahmedah976@gmail.com>
2025-12-11 16:29:20 +0530

    feat : form layout component component added

---
+++ components/ui/form-layout.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/lib/utils"
+import React, { ReactNode } from "react"
+
+type FormLayoutProps = {
+  className?: string
+  headerClassName?: string
+  footerClassName?: string
+  header?: ReactNode
+  children: ReactNode
+  footer?: ReactNode
+}
+
+const FormLayout = ({
+  children,
+  footer,
+  header,
+  className,
+  footerClassName,
+  headerClassName,
+}: FormLayoutProps) => {
+  return (
+    <div
+      className={cn(
+        "rounded-lg border-border border bg-background w-full h-full grid grid-rows-[auto_60px]",
+        header && "grid-rows-[auto_auto_60px]"
+      )}
+    >
+      {header && (
+        <div className={cn("px-4 py-2 border-b flex items-center justify-end", headerClassName)}>
+          {header}
+        </div>
+      )}
+      <div className={cn("p-6 h-full grid grid-cols-2 overflow-y-scroll gap-4", className)}>
+        {children}
+      </div>
+      <div className={cn("px-4 border-t flex items-center justify-end", footerClassName)}>
+        {footer}
+      </div>
+    </div>
+  )
+}
+
+export default FormLayout
