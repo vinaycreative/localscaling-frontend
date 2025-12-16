7b8166868ebfee34cd804f6278ce323ad3632c9b
AdilAhmed976 <adilahmedah976@gmail.com>
2025-12-16 17:13:39 +0530

    fix : Property 'CLIENT' does not exist on type issue fix

--- store/auth.ts
+++ store/auth.ts
@@ -29,7 +29,7 @@ export const useAuthStore = create<AuthState>((set, get) => ({
     return arr.includes(current)
   },
   canAccess: (pathname) => {
-    const role = get().user?.role ?? Role.CLIENT
+    const role = get().user?.role ?? Role.client
     return canAccessPath(role, pathname)
   },
 }))
