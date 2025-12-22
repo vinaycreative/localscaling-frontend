bc8b04604c53b0170fe6ad875a5107ea69ac3863
AdilAhmed976 <adilahmedah976@gmail.com>
2025-12-22 13:40:07 +0530

    fix : font_link schema fix

--- form/branding-content/schema.ts
+++ form/branding-content/schema.ts
@@ -3,11 +3,11 @@ import z from "zod"
 const hexColorRegex = /^#([0-9A-F]{3}){1,2}$/i
 
 export const brandingContentSchema = z.object({
-  font_link: z.union([z.literal(""), z.string().url({ message: "Invalid Font URL" })]),
- primary_brand_color: z
+  font_link: z.union([z.string().url({ message: "Invalid Font URL" })]),
+  primary_brand_color: z
     .string()
     .regex(hexColorRegex, { message: "Invalid Primary Brand Color (Hex)" }),
-  secondary_brand_color:z
+  secondary_brand_color: z
     .string()
     .regex(hexColorRegex, { message: "Invalid Secondary Brand Color (Hex)" }),
   logo_file: z.any().nullable(),
